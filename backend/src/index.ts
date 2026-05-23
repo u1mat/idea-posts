b31624e84069ecd94d24a11c48da3e5e2a18ba14
Урмат Коконбаев <urmatkokonbaev@MacBook-Pro-Urmat.local>
2026-05-24 01:24:36 +0600

    init: trpc webapp

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,37 @@
 import express from "express";
+import cors from "cors";
 import * as trpcExpress from "@trpc/server/adapters/express";
-
 import { trpcRouter } from "./trpc";
 
 const app = express();
 const PORT = 3000;
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "trpc-accept"],
+};
+
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, trpc-accept",
+  );
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(204);
+    return;
+  }
+
+  next();
+  return;
+});
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,31 @@
-import { Button } from "./components/ui/button"
+import { TrpcProvider } from "./lib/trpc";
+import { trpc } from "./lib/trpc";
+
+function AppContent() {
+  const { data, error, isLoading, isError } = trpc.getIdeas.useQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  
+  if (isError) return <div>Error occurred: {error.message}</div>;
+
+  return (
+    <div className="App">
+      {data?.map((idea) => (
+        <div key={idea.id}>
+          <h2>{idea.title}</h2>
+          <p>{idea.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <Button>Click</Button>
-    </div>
-  )
+    <TrpcProvider>
+      <AppContent />
+    </TrpcProvider>
+  );
 }
 
-export default App
+export default App;
